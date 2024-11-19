0fe8f9a4c82d197368882f91bad07339a50fb7ea
Monisankar <mxn170301@darden.com>
2024-11-19 11:44:47 +0530

    Initial Commit

---
+++ IosYTApi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IosYTApi
+//
+//  Created by Monisankar Nath on 19/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IosYTApi/IosYTApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  IosYTApiApp.swift
+//  IosYTApi
+//
+//  Created by Monisankar Nath on 19/11/24.
+//
+
+import SwiftUI
+
+@main
+struct IosYTApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
